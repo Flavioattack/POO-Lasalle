44a1fe66e225710442b7233e482f5d92c4f0ed36
Flavioattack <54382417+Flavioattack@users.noreply.github.com>
2021-08-17 19:39:15 -0300

    Add files via upload

---
+++ multiplicacao.java
@@ -0,0 +1,10 @@
+/**
+ * mat:0050017196 - Flávio Henrique
+ */
+public class multiplicacao {
+    public static void main(String[] args) {
+       for(int i = 0; i <= 10; i++){
+           System.out.println(i*10);
+       }
+    }
+}
